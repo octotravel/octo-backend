eba2dc23d1380e36829fd9ddd31bdb973c950868
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-04-09 17:21:54 +0200

    wip

--- src/api/customFetchRetry.ts
+++ src/api/customFetchRetry.ts
@@ -21,77 +21,85 @@ export async function customFetchRetry(
   init?: RequestInit,
   options: FetchRetryOptions = FETCH_RETRY_DEFAULT_OPTIONS,
 ): Promise<Response> {
-  let {
-    subRequestContext = null,
-    currentRetryAttempt = 0,
-    maxRetryAttempts = DEFAULT_MAX_RETRY_ATTEMPTS,
-    retryDelayMultiplierInMs = DEFAULT_RETRY_DELAY_MULTIPLIER_IN_MS,
-  } = options;
-  let subRequestRetryContext: SubRequestRetryContext | null = null;
+  try {
+    let {
+      subRequestContext = null,
+      currentRetryAttempt = 0,
+      maxRetryAttempts = DEFAULT_MAX_RETRY_ATTEMPTS,
+      retryDelayMultiplierInMs = DEFAULT_RETRY_DELAY_MULTIPLIER_IN_MS,
+    } = options;
+    let subRequestRetryContext: SubRequestRetryContext | null = null;
 
-  if (currentRetryAttempt > 0) {
-    if (subRequestContext !== null) {
-      const request: Request = input instanceof Request ? input : new Request(input, init);
+    if (currentRetryAttempt > 0) {
+      if (subRequestContext !== null) {
+        const request: Request = input instanceof Request ? input : new Request(input, init);
 
-      subRequestRetryContext = new SubRequestRetryContext({
-        request: request.clone(),
-        accountId: subRequestContext.getAccountId(),
-        requestId: subRequestContext.getRequestId(),
-        subRequestId: subRequestContext.getId(),
+        subRequestRetryContext = new SubRequestRetryContext({
+          request: request.clone(),
+          accountId: subRequestContext.getAccountId(),
+          requestId: subRequestContext.getRequestId(),
+          subRequestId: subRequestContext.getId(),
+        });
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, (currentRetryAttempt + 1) * retryDelayMultiplierInMs));
+    }
+
+    let res: Response | undefined;
+    let error: Error | null = null;
+
+    try {
+      res = await fetch(input, init);
+    } catch (e: any) {
+      console.log(e);
+      res = new Response('{}', { status: 500 });
+
+      if (e instanceof Error) {
+        error = e;
+      }
+
+      throw e;
+    }
+
+    if (currentRetryAttempt === 0 && subRequestContext !== null) {
+      subRequestContext.setResponse(res);
+      subRequestContext.setError(error);
+    } else if (currentRetryAttempt > 0 && subRequestContext !== null && subRequestRetryContext !== null) {
+      subRequestRetryContext.setResponse(res);
+      subRequestRetryContext.setError(error);
+      const requestData = subRequestRetryContext.getRequestData();
+      subRequestContext.addRetry(requestData.clone());
+    }
+
+    currentRetryAttempt++;
+
+    if (res === undefined && currentRetryAttempt < maxRetryAttempts) {
+      return await customFetchRetry(input, init, {
+        subRequestContext,
+        currentRetryAttempt,
+        maxRetryAttempts,
+        retryDelayMultiplierInMs,
       });
     }
 
-    await new Promise((resolve) => setTimeout(resolve, (currentRetryAttempt + 1) * retryDelayMultiplierInMs));
-  }
+    const status = res.status;
 
-  let res: Response | undefined;
-  let error: Error | null = null;
-
-  try {
-    res = await fetch(input, init);
-  } catch (e: any) {
-    res = new Response('{}', { status: 500 });
-
-    if (e instanceof Error) {
-      error = e;
+    if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
+      return await customFetchRetry(input, init, {
+        subRequestContext,
+        currentRetryAttempt,
+        maxRetryAttempts,
+        retryDelayMultiplierInMs,
+      });
     }
+
+    if (error !== null) {
+      throw error;
+    }
+
+    return res.clone();
+  } catch (e: any) {
+    console.log(e);
+    throw e;
   }
-
-  if (currentRetryAttempt === 0 && subRequestContext !== null) {
-    subRequestContext.setResponse(res);
-    subRequestContext.setError(error);
-  } else if (currentRetryAttempt > 0 && subRequestContext !== null && subRequestRetryContext !== null) {
-    subRequestRetryContext.setResponse(res);
-    subRequestRetryContext.setError(error);
-    const requestData = subRequestRetryContext.getRequestData();
-    subRequestContext.addRetry(requestData.clone());
-  }
-
-  currentRetryAttempt++;
-
-  if (res === undefined && currentRetryAttempt < maxRetryAttempts) {
-    return await customFetchRetry(input, init, {
-      subRequestContext,
-      currentRetryAttempt,
-      maxRetryAttempts,
-      retryDelayMultiplierInMs,
-    });
-  }
-
-  const status = res.status;
-
-  if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
-    return await customFetchRetry(input, init, {
-      subRequestContext,
-      currentRetryAttempt,
-      maxRetryAttempts,
-      retryDelayMultiplierInMs,
-    });
-  }
-
-  if (error !== null) {
-    throw error;
-  }
-
-  return res.clone();
 }
