6b195aa02863ab7514fad80ce9ef98bf31b33a91
Michal Panský <pansky.michal@gmail.com>
2025-06-09 18:36:53 +0200

    fix: remove unnecessary blank line in getAvailability method

--- src/services/AvailabilityService.ts
+++ src/services/AvailabilityService.ts
@@ -29,7 +29,7 @@ export class AvailabilityService implements IAvailabilityService {
     if (params.useRawUnits) {
       return availabilities;
     }
-    
+
     return UnitHelper.updateWithFilteredFirstUnitPricing(availabilities);
   };
 
